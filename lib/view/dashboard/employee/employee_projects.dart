439889b884839c1240339e7ba9c8c19afe346bce
clode <cloderaldomorales@gmail.com>
2021-05-20 11:11:48 +0800

    employee project list redesign

--- lib/view/dashboard/employee/employee_projects.dart
+++ lib/view/dashboard/employee/employee_projects.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:uitemplate/config/global.dart';
 import 'package:uitemplate/config/pallete.dart';
 import 'package:uitemplate/models/employes_model.dart';
 import 'package:uitemplate/models/user_project_model.dart';
@@ -19,12 +20,18 @@ class EmployeeProjectsDetails extends StatefulWidget {
 
 class _EmployeeProjectsDetailsState extends State<EmployeeProjectsDetails>
     with SettingsHelper {
+  bool loader = false;
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance!.addPostFrameCallback((_) {
       Provider.of<EmployeeSevice>(context, listen: false)
-          .workingProjects(widget.emplyoyee!.id!);
+          .workingProjects(widget.emplyoyee!.id!)
+          .whenComplete(() {
+        setState(() {
+          loader = true;
+        });
+      });
     });
   }
 
@@ -40,68 +47,108 @@ class _EmployeeProjectsDetailsState extends State<EmployeeProjectsDetails>
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Palette.contentBackground,
-                    backgroundImage:
-                        fetchImage(netWorkImage: widget.emplyoyee!.picture),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                        "${widget.emplyoyee!.fname} ${widget.emplyoyee!.lname}"),
-                  )
-                ],
+            Container(
+              decoration: BoxDecoration(
+                  color: Palette.drawerColor,
+                  borderRadius: BorderRadius.circular(5)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      backgroundImage:
+                          fetchImage(netWorkImage: widget.emplyoyee!.picture),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                        "${widget.emplyoyee!.fname} ${widget.emplyoyee!.lname}",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      "Total Projets : ${employeeService.employeeProjects != null && employeeService.employeeProjects!.length > 0 ? employeeService.employeeProjects!.length : 0} ",
+                      style: TextStyle(color: Colors.white, fontSize: 10),
+                    )
+                  ],
+                ),
               ),
             ),
-            employeeService.employeeProjects != null
-                ? Expanded(
-                    child: ListView(
-                      children: [
-                        employeeService.employeeProjects!.length > 0
-                            ? Text("Projets Actuels")
-                            : SizedBox(),
-                        for (UserProjectModel project
-                            in employeeService.employeeProjects!)
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Consumer<ColorChangeService>(
-                                    builder: (context, data, child) {
-                                      return Icon(
-                                        Icons.circle,
-                                        color: data.statusColors[
-                                            project.userProject!.status!],
-                                        size: 15,
-                                      );
-                                    },
-                                  ),
-                                  SizedBox(
-                                    width: MySpacer.small,
-                                  ),
-                                  Flexible(
-                                    child: Text(
-                                      "${project.userProject!.name}",
-                                      overflow: TextOverflow.clip,
-                                    ),
-                                  ),
-                                ],
+            employeeService.employeeProjects != null && loader
+                ? employeeService.employeeProjects!.length == 0
+                    ? Expanded(
+                        child: Container(
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                "assets/images/empty.png",
+                                width: 100,
                               ),
-                            ),
+                              Text("No projects yet!"),
+                            ],
                           ),
-                        SizedBox(
-                          height: MySpacer.medium,
                         ),
-                        // Text("Projet précédent"),
-                      ],
-                    ),
-                  )
+                      ))
+                    : Expanded(
+                        child: ListView(
+                          children: [
+                            employeeService.employeeProjects!.length > 0
+                                ? Padding(
+                                    padding:
+                                        const EdgeInsets.symmetric(vertical: 5),
+                                    child: Text(
+                                      "Projets Actuels",
+                                      style: boldText,
+                                    ),
+                                  )
+                                : SizedBox(),
+                            for (UserProjectModel project
+                                in employeeService.employeeProjects!)
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(3)),
+                                margin: EdgeInsets.only(bottom: 5),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Consumer<ColorChangeService>(
+                                        builder: (context, data, child) {
+                                          return Icon(
+                                            Icons.circle,
+                                            color: data.statusColors[
+                                                project.userProject!.status!],
+                                            size: 15,
+                                          );
+                                        },
+                                      ),
+                                      SizedBox(
+                                        width: MySpacer.small,
+                                      ),
+                                      Flexible(
+                                        child: Text(
+                                          "${project.userProject!.name}",
+                                          overflow: TextOverflow.clip,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            SizedBox(
+                              height: MySpacer.medium,
+                            ),
+                            // Text("Projet précédent"),
+                          ],
+                        ),
+                      )
                 : Expanded(
                     child: Center(
                       child: CircularProgressIndicator(),
